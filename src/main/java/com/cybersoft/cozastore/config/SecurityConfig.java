09d0f25b8d9e279d11a1c78a590848921bd09a9d
Nguyen Cuong <nqc@gmail.com>
2023-05-24 20:04:32 +0700

    second commit

--- src/main/java/com/cybersoft/cozastore/config/SecurityConfig.java
+++ src/main/java/com/cybersoft/cozastore/config/SecurityConfig.java
@@ -1,41 +1,52 @@
 package com.cybersoft.cozastore.config;
 
+import com.cybersoft.cozastore.filter.JWTFilter;
+import com.cybersoft.cozastore.provider.CustomAuthenManagerProvider;
 import com.cybersoft.cozastore.service.CustomUserServiceDetail;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    CustomAuthenManagerProvider customAuthenManagerProvider;
+    @Autowired
+    JWTFilter jwtFilter;
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
 
         return new BCryptPasswordEncoder();
     }
-
+    //  Tao AuthenticationManager de lay thong tin chung thuc
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception{
 
         CustomUserServiceDetail customUserDetailService = new CustomUserServiceDetail();
 
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(customUserDetailService)
-                .passwordEncoder(passwordEncoder())
-                .and().build();
+                .authenticationProvider(customAuthenManagerProvider)
+                .build();
 
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
 //                Cho phép cấu hình chứng thực cho các request
                 .authorizeHttpRequests()
 //                   Quy định chứng thực cho link chỉ định
@@ -43,7 +54,9 @@ public class SecurityConfig {
 
 //                   Tất cả các link còn lại đều phải chứng thực
                 .anyRequest().authenticated()
-                .and().httpBasic().and().build();
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
     }
 
--- src/main/java/com/cybersoft/cozastore/utils/JWTHelperUtils.java
+++ src/main/java/com/cybersoft/cozastore/utils/JWTHelperUtils.java
@@ -1,22 +1,49 @@
 package com.cybersoft.cozastore.utils;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 
 @Component
 public class JWTHelperUtils {
+    // @Value : Giúp lấy key khai báo bên file application.properties
+    @Value("${jwt.token.key}")
+    String secretKey;
+
+    /**
+     * Bước 1 : Tạo key
+     * Bước 2 : Sử dụng key mới tạo để sinh ra tokem
+     */
 
     public String generateToken(String data){
 
-        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        String key = Encoders.BASE64.encode(secretKey.getEncoded());
-        System.out.println(key);
+//        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//        String key = Encoders.BASE64.encode(secretKey.getEncoded());
+//        System.out.println(key);
 
-        return "";
+        // Lấy secretKey đã tạo trước đó để sử dụng
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+
+        // Dùng key để tạo ra token
+        String token = Jwts.builder().setSubject(data).signWith(key).compact();
+
+        return token;
+    }
+
+    public String validToken(String token){
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+//        Chuẩn bị chìa khóa để tiến hành giải mã
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token) //Truyền token cần giải mã
+                .getBody().getSubject();
+
     }
 
 }
+
