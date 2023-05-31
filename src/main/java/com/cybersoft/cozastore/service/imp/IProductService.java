8a7ea6c40738b8b761b2bd31880169a1bf587367
Nguyen Cuong <nqc@gmail.com>
2023-05-31 20:56:06 +0700

    API Get Product By Category Id

---
+++ src/main/java/com/cybersoft/cozastore/service/imp/IProductService.java
@@ -0,0 +1,12 @@
+package com.cybersoft.cozastore.service.imp;
+
+import com.cybersoft.cozastore.payload.response.ProductResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface IProductService {
+
+     List<ProductResponse> getProductByCategoryId(int id);
+
+}
